30a9d4ad4fd349fcdeef8bf270af6151cbd32494
priderc <r.contrerasgalvez@gmail.com>
2017-08-02 23:08:09 -0400

    Change

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ import time
 import json
 
 #consumer key, consumer secret, access token, access secret.
-ckey="vzesZnrNCRkT638aJg8zA1iw7"
-csecret="9u0UdGmKd7NjATMqqBjJxKDTZuxjlSB8tEkDM9YXZ4YY6onVOw"
-atoken="3429693021-7btKN1A4TJpSX95FABMWnDH91qjjdbb5lc85fsZ"
-asecret="vEBn0KQ9SlHOi3mdACFfqdHLLGYbUW45OkZ9CTm9cQLZq"
+ckey=""
+csecret=""
+atoken=""
+asecret=""
 
 class listener(StreamListener):
 
